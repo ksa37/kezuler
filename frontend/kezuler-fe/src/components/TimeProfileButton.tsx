5561b27c9bdd76f86aefc6197e311647458a4cea
ksa37 <ksa37@kaist.ac.kr>
2022-05-20 17:24:51 +0900

    update: TimeProfileButtonList but need modification

--- frontend/kezuler-fe/src/components/TimeProfileButton.tsx
+++ frontend/kezuler-fe/src/components/TimeProfileButton.tsx
@@ -5,29 +5,50 @@ import { PossibleUser } from '../types/pendingEvent';
 
 interface Props {
   onClick: () => void;
-  time: string;
+  eventStartsAt: string;
+  eventTimeDuration: number;
   possibleUsersList?: PossibleUser[];
 }
 
-function TimeProfileButton({ onClick, time, possibleUsersList }: Props) {
+function TimeProfileButton({
+  onClick,
+  eventStartsAt,
+  possibleUsersList,
+}: Props) {
   const possibleUsersNum = possibleUsersList?.length;
   const maxAvatarNum = 3;
 
+  //duration 계산하는거 넣어야함
+  // const getStartHour = eventStartsAt.split(':')[0];
+  // const getStartMinute = eventStartsAt.split(':')[1];
+  // let endTime = '';
+  // switch(getStartMinute){
+  //   case '00':
+  //     endTime = getStartHour + ':' + '30';
+  //           break;
+  //   case '30'
+  //   endTime = getStartHour + ':' + '30';
+
+  //     break;
+  //   default:
+  //     console.log('eventStartsAt time error');
+  // }
+
   return (
     <button className="time-profile-button" onClick={onClick}>
-      {time}
+      {eventStartsAt}
       {possibleUsersNum}명 참여중
-      <AvatarGroup max={maxAvatarNum} total={possibleUsersNum}>
+      {/* <AvatarGroup max={maxAvatarNum} total={possibleUsersNum}>
         {possibleUsersList?.map((possibleUser) => {
           return (
             <Avatar
               key={possibleUser.userId}
               alt={possibleUser.userId}
-              src={possibleUser.userImage}
+              src={possibleUser.userProfileImage}
             />
           );
         })}
-      </AvatarGroup>
+      </AvatarGroup> */}
     </button>
   );
 }
