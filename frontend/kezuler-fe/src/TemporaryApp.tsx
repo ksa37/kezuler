318befc2ec6ba612758f16b80d01b619f7b73f38
ksa37 <ksa37@kaist.ac.kr>
2022-05-27 14:45:30 +0900

    update: for notitalk

--- frontend/kezuler-fe/src/TemporaryApp.tsx
+++ frontend/kezuler-fe/src/TemporaryApp.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 
-import RootRoutes from './routes/RootRoutes';
-
 import './App.scss';
 
 function TemporaryApp() {
   return (
     <div>
-      <header>Kezuler</header>
-      <footer>
+      <header
+        style={{
+          fontSize: 16,
+          textAlign: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <b>Kezuler</b>
+      </header>
+      <footer
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          fontSize: 12,
+          backgroundColor: 'lightgray',
+        }}
+      >
         (주)올렌다 대표이사 구자룡 서울특별시 성북구 오패산로3길 136-12(청담동)
         사업자 등록번호 736-87-01642
       </footer>
--- frontend/kezuler-fe/src/index.tsx
+++ frontend/kezuler-fe/src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 
-import { store } from './store/store';
-
-import App from './App';
+// import { Provider } from 'react-redux';
+// import { BrowserRouter } from 'react-router-dom';
+// import { store } from './store/store';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import TemporaryApp from './TemporaryApp';
 
