95841d9a9d31c08182ae2c5ab92d2072a5df5f3b
ksa37 <ksa37@kaist.ac.kr>
2022-05-30 03:48:47 +0900

    add: acceptanceCompletion style

--- frontend/kezuler-fe/src/views/accept-meeting/AcceptanceCompletion.tsx
+++ frontend/kezuler-fe/src/views/accept-meeting/AcceptanceCompletion.tsx
@@ -5,6 +5,9 @@ import PathName from 'src/constants/PathName';
 
 import BottomButton from 'src/components/common/BottomButton';
 
+import { ReactComponent as Bell } from 'src/assets/acceptance_complete_bell.svg';
+import { ReactComponent as Circle } from 'src/assets/acceptance_complete_circle.svg';
+
 function AcceptanceCompletion() {
   const navigate = useNavigate();
 
@@ -17,11 +20,21 @@ function AcceptanceCompletion() {
   };
 
   return (
-    <>
-      <h2>{completeMainDescription}</h2>
-      <h3>{completeSubDescription}</h3>
+    <div className={'acceptance-completion'}>
+      <div className={'description-text'}>
+        {'일정 선택이'}
+        <br />
+        {'모두 완료되었습니다!'}
+      </div>
+      <div className={'acceptance-completion-sub-description'}>
+        {'호스트가 미팅시간을 확정하면'}
+        <br />
+        {'다시 알려드릴게요.'}
+      </div>
+      <Bell className={'acceptance-bell'} />
+      <Circle className={'acceptance-circle'} />
       <BottomButton onClick={handleHomeClick} text={'홈으로 가기'} />
-    </>
+    </div>
   );
 }
 
