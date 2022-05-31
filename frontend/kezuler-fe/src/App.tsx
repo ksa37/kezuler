cddae6efa75e03402718d432f970d554f13bf965
Jongwon Jang <jangjwhw@gmail.com>
2022-05-31 21:06:36 +0900

    ADD Kezuler Dialog and ADD StyledEngineProvider for css order

--- frontend/kezuler-fe/src/App.tsx
+++ frontend/kezuler-fe/src/App.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 import RootRoutes from './routes/RootRoutes';
 
 import KezulerModal from './components/modal/KezulerModal';
+import KezulerDialog from 'src/components/modal/KezulerDialog';
 
 import 'src/styles/index.scss';
+import 'src/styles/dialog.scss';
 
 function App() {
   return (
     <div className="App">
       <RootRoutes />
       <KezulerModal />
+      <KezulerDialog />
     </div>
   );
 }
