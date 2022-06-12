d6aa149edded64640e8bffe393e7ee1e0ef1e089
ksa37 <ksa37@kaist.ac.kr>
2022-06-13 03:23:47 +0900

    add: styles from accept-meeting so code organization needed

--- frontend/kezuler-fe/src/views/create-meeting/SelectedOptions.tsx
+++ frontend/kezuler-fe/src/views/create-meeting/SelectedOptions.tsx
@@ -9,6 +9,8 @@ import { getTimeListDevideByDate, getTimeRange } from 'src/utils/dateParser';
 
 import BottomButton from 'src/components/common/BottomButton';
 
+import { ReactComponent as DeleteIcon } from 'src/assets/icn_trash.svg';
+
 function ShowSelectedOptions() {
   const dispatch = useDispatch<AppDispatch>();
   const { increaseStep, deleteTimeList } = createMeetingActions;
@@ -35,28 +37,37 @@ function ShowSelectedOptions() {
   };
 
   return (
-    <div>
-      <div className={'description-text'}>
-        {'선택한 날짜와 시간을'}
-        <br />
-        {'확인해주세요'}
+    <div className={'time-list-selector'}>
+      <div className={'time-list-top'}>
+        <div className={'time-list-top-description'}>
+          {'선택한 날짜와 시간을'}
+          <br />
+          {'확인해주세요'}
+        </div>
+        <div className={'time-list-selector-personnel'}>{subDescription}</div>
       </div>
-      <div className={'selected-num'}>{subDescription}</div>
-      <div>
+      <div className={'time-select-with-schedule'}>
+        <div className={'time-line-line'} />
         {Object.keys(eventTimeListDevideByDate).map((dateKey) => (
-          <div key={dateKey}>
-            <div className={'timelineLine'}></div>
-            <div>
-              <div className={'timelineCircle'}></div>
-              {dateKey}
+          <div key={dateKey} className={'time-select-date'}>
+            <div className={'time-select-date-grid'}>
+              <div className={'time-select-date-part'}>
+                <div className={'time-line-circle'} />
+                {dateKey}
+              </div>
             </div>
             {eventTimeListDevideByDate[dateKey].map((time) => (
-              <div key={dateKey + time}>
-                <div>
-                  {getTimeRange(time, eventTimeDuration)}
-                  <Button onClick={() => handleDeleteClick(dateKey, time)}>
-                    X
-                  </Button>
+              <div key={dateKey + time} className={'time-select-card-grid'}>
+                <div className={'time-select-time-card'}>
+                  <div className={'time-select-time-content'}>
+                    {getTimeRange(time, eventTimeDuration)}
+                  </div>
+                  <div
+                    className="check-box-icon"
+                    onClick={() => handleDeleteClick(dateKey, time)}
+                  >
+                    <DeleteIcon />
+                  </div>
                 </div>
               </div>
             ))}
