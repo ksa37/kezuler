cc9d5876c440ecbcc0157ca93adeff07c4fe1b8d
ksa37 <ksa37@kaist.ac.kr>
2022-06-18 05:57:25 +0900

    update: KezulerDialog, when confirm

--- frontend/kezuler-fe/src/components/modal/KezulerDialog.tsx
+++ frontend/kezuler-fe/src/components/modal/KezulerDialog.tsx
@@ -19,7 +19,8 @@ function KezulerDialog() {
     return null;
   }
 
-  const { date, title, description, onCancel, onConfirm } = dialogProps;
+  const { date, timeRange, title, description, onCancel, onConfirm } =
+    dialogProps;
 
   // 아무 조작하지 않고 닫았을 때 (모달 밖 클릭, X 버튼 클릭 등)
   const handleClose = () => {
@@ -45,6 +46,8 @@ function KezulerDialog() {
       <div className={'kezuler-dialog-body'}>
         <div className={'kezuler-dialog-date-wrapper'}>
           {date && <h1 className={'date'}>{date}</h1>}
+          {timeRange && <br />}
+          {timeRange && <h1 className={'date'}>{`${timeRange}`}</h1>}
         </div>
         <h1>{title}</h1>
         {description && <h2>{description}</h2>}
--- frontend/kezuler-fe/src/views/pending-event/TimeConfirmator.tsx
+++ frontend/kezuler-fe/src/views/pending-event/TimeConfirmator.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { format } from 'date-fns';
 
 import { ConfirmMeetingSteps } from 'src/constants/Steps';
 import useDialog from 'src/hooks/useDialog';
@@ -65,14 +66,19 @@ function TimeConfirmator() {
       dispatch(increaseStep());
     };
 
+    const selectedDateText = format(new Date(selectedTime), 'yyyy년 M월 d일');
+    const selectedTimeText = getTimeRange(
+      new Date(selectedTime),
+      eventTimeDuration
+    );
+
     openDialog({
-      date: '2022년 6월 1일',
+      date: `${selectedDateText}`,
+      timeRange: `${selectedTimeText}`,
       title: '미팅시간을 최종 확정하시겠어요?',
       description: '확정 시, 참여자들에게\n카카오톡 메세지가 전송됩니다.',
       onConfirm: handleConfirm,
     });
-
-    // console.log('post!');
   };
 
   const handlePrevClick = () => {
