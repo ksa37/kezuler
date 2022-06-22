9f263d1e35edb3c3e6516fb7671dbdcbe507045e
ksa37 <ksa37@kaist.ac.kr>
2022-06-22 15:34:54 +0900

    fix: current host

--- frontend/kezuler-fe/src/constants/Oauth.ts
+++ frontend/kezuler-fe/src/constants/Oauth.ts
@@ -1,4 +1,4 @@
-const CURRENT_HOST = 'http://localhost:3000';
+const CURRENT_HOST = 'http://kezuler.com';
 
 const CLIENT_ID = '0c7841de042de4b73e3a11c1af2f6671';
 const REDIRECT_URI = `${CURRENT_HOST}/oauth/callback/kakao`;
