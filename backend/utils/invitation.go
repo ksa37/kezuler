c63cb678233a128431ac9bf09ad98830c81fcbeb
hojunee <relativity1915@gmail.com>
2022-06-22 11:00:51 +0900

    fix: route and logic to add invitation for FixedEvent

--- backend/utils/invitation.go
+++ backend/utils/invitation.go
@@ -9,20 +9,52 @@ import (
 	"net/http"
 )
 
-func getInvitationInfo(w http.ResponseWriter, peId string) {
+func getPendingEventInvitationWithInfo(w http.ResponseWriter, peId string) {
 	client := connect()
 	defer disconnect(client)
 
 	pendingEventCol := client.Database("kezuler").Collection("pendingEvent")
 	var targetEvent PendingEvent
-	err := pendingEventCol.FindOne(context.TODO(), bson.D{{"pendingEventId", peId}}).Decode(&targetEvent)
+	err := pendingEventCol.FindOne(context.TODO(), bson.M{"pendingEventId": peId}).Decode(&targetEvent)
 	if err == mongo.ErrNoDocuments {
 		fmt.Printf("No Document was found with given peId: %s\n", peId)
-		http.Error(w, "Not Found", http.StatusNotFound)
+		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
 
-	jsonRes, err := json.Marshal(targetEvent)
+	targetEventWithInfo, err := GetInfoInPendingEvent(client, targetEvent)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	jsonRes, err := json.Marshal(targetEventWithInfo)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonRes)
+}
+
+func getFixedEventInvitationWithInfo(w http.ResponseWriter, feId string) {
+	client := connect()
+	defer disconnect(client)
+
+	fixedEventCol := client.Database("kezuler").Collection("fixedEvent")
+	var targetEvent FixedEvent
+
+	err := fixedEventCol.FindOne(context.TODO(), bson.D{{"fixedEventId", feId}}).Decode(&targetEvent)
+	if err != nil {
+		fmt.Printf("No Document was found with given feId: %s\n", feId)
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	targetEventWithInfo, err := GetInfoInFixedEvent(client, targetEvent)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	jsonRes, err := json.Marshal(targetEventWithInfo)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(jsonRes)
