0dd48a65d805dadbe9fdbd6e154167f92d29e51e
ksa37 <ksa37@kaist.ac.kr>
2022-06-24 17:50:30 +0900

    update: remove kezuler Intro when loggedIn

--- frontend/kezuler-fe/src/components/KezulerIntro.tsx
+++ frontend/kezuler-fe/src/components/KezulerIntro.tsx
@@ -1,32 +1,21 @@
-import React from 'react';
-import { Button } from '@mui/material';
+import React, { useMemo } from 'react';
 
-import { KAKAO_AUTH_URL } from 'src/constants/Auth';
+import { ACCESS_TOKEN_KEY } from 'src/constants/Auth';
+import { getCookie } from 'src/utils/cookie';
 
-import KakaoIcon from 'src/assets/img_kakao.svg';
-import PCIntro from 'src/assets/pc_intro.svg';
-// import 'src/styles/login.scss';
+import { ReactComponent as PCIntro } from 'src/assets/pc_intro.svg';
 import 'src/styles/index.scss';
 
 function KezulerIntro() {
+  const isLoggedIn = useMemo(() => !!getCookie(ACCESS_TOKEN_KEY), []);
   return (
-    <div className={'kezuler-intro'}>
-      <img src={PCIntro} alt="1" />
-      {/* <PCIntro /> */}
-      {/* <div>
-        <Button
-          // href={KAKAO_AUTH_URL}
-          classes={{ root: 'kezuler-intro-login-kakao-btn' }}
-        >
-          <img
-            src={KakaoIcon}
-            alt="1"
-            className={'kezuler-intro-login-kakao-icn'}
-          />
-          <div>카카오로 원클릭 로그인!</div>
-        </Button>
-      </div> */}
-    </div>
+    <>
+      {!isLoggedIn && (
+        <div id="kezuler-intro" className={'kezuler-intro'}>
+          <PCIntro className={'kezuler-intro-img'} />
+        </div>
+      )}
+    </>
   );
 }
 
