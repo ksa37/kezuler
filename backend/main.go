f07c83fef829cfcf60e4fb81fa842303590a54f4
hojunee <relativity1915@gmail.com>
2022-06-28 01:35:36 +0900

    fix: remove legacy code & add: support for saving PhoneNumber in POST /user

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
-	"io"
 	"kezuler/utils"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
-	"strings"
 )
 
 // ref: https://setapp.com/how-to/use-go-with-mongodb
@@ -40,55 +35,6 @@ func main() {
 
 	logMiddleWare := utils.NewLogMiddleware(logger)
 	mainRouter.Use(logMiddleWare.Func())
-
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "pong")
-	})
-
-	// EventHandler for Redirect. Get Kakao AuthToken and pass it to /login/kakao
-	http.HandleFunc("/oauth/kakao/token", func(w http.ResponseWriter, r *http.Request) {
-		// 1. 받은 토큰 검증
-		code := r.URL.Query().Get("code")
-		if code == "" {
-			fmt.Fprintf(w, "Error: 'code' parameter doesn't exist on query string")
-		}
-
-		payload := url.Values{
-			"grant_type":   {"authorization_code"},
-			"client_id":    {"0c7841de042de4b73e3a11c1af2f6671"},
-			"redirect_uri": {"http://localhost:3000/oauth/kakao/token"},
-			"code":         {code},
-		}
-
-		const ClientURI string = "https://kauth.kakao.com"
-		const Resource string = "/oauth/token"
-
-		// generate endpoint
-		u, _ := url.ParseRequestURI(ClientURI)
-		u.Path = Resource
-		urlStr := u.String()
-
-		client := &http.Client{}
-		req, _ := http.NewRequest(http.MethodPost, urlStr, strings.NewReader(payload.Encode()))
-		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-		req.Header.Add("charset", "utf-8")
-
-		res, err := client.Do(req)
-		if err != nil {
-			panic(err)
-		}
-
-		body, err := io.ReadAll(res.Body)
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Fprintf(w, string(body))
-		jsonBody := utils.KakaoOAuthTokenResponse{}
-		json.Unmarshal(body, &jsonBody)
-
-		fmt.Printf(jsonBody.AccessToken)
-	})
 
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
