a8d38a0b7a289c65b2d277ac4824e2948a1d8a93
ksa37 <ksa37@kaist.ac.kr>
2022-07-24 18:25:46 +0900

    add: KezulerAlert component and reducer

---
+++ frontend/kezuler-fe/src/components/modal/KezulerAlert.tsx
@@ -0,0 +1,48 @@
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Dialog from '@mui/material/Dialog';
+
+import { RootState } from 'src/reducers';
+import { alertAction } from 'src/reducers/alert';
+
+import { ReactComponent as CloseIcon } from 'src/assets/icn_close_b.svg';
+
+function KezulerAlert() {
+  const { hide } = alertAction;
+  const dispatch = useDispatch();
+
+  const closeModal = useCallback(() => {
+    dispatch(hide());
+  }, [dispatch, hide]);
+
+  const { alertProps } = useSelector((state: RootState) => state.alert);
+
+  if (!alertProps) {
+    return null;
+  }
+
+  const { title, description } = alertProps;
+
+  // 아무 조작하지 않고 닫았을 때 (모달 밖 클릭, X 버튼 클릭 등)
+  const handleClose = () => {
+    closeModal();
+  };
+
+  return (
+    <Dialog
+      open={!!alertProps}
+      onClose={handleClose}
+      classes={{ paper: 'kezuler-dialog' }}
+    >
+      <button className={'overview-close-btn'} onClick={closeModal}>
+        <CloseIcon />
+      </button>
+      <div className={'kezuler-dialog-body'}>
+        <h1>{title}</h1>
+        {description && <h2>{description}</h2>}
+      </div>
+    </Dialog>
+  );
+}
+
+export default KezulerAlert;
