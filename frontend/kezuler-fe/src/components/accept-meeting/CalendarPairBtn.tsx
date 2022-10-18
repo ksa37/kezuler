4128bd665707a3a76379b11cd5a9ec11187caac3
ksa37 <ksa37@kaist.ac.kr>
2022-10-18 11:53:42 +0900

    add: google account connect popup noticing putting all schedules

--- frontend/kezuler-fe/src/components/accept-meeting/CalendarPairBtn.tsx
+++ frontend/kezuler-fe/src/components/accept-meeting/CalendarPairBtn.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useGoogleLogin } from '@react-oauth/google';
 
 import { GOOGLE_LOGIN_SCOPE } from 'src/constants/Auth';
+import useDialog from 'src/hooks/useDialog';
 import useGetUserInfo from 'src/hooks/useGetUserInfo';
 import { usePatchUser } from 'src/hooks/usePatchUser';
 
@@ -16,6 +17,7 @@ interface Props {
 function CalendarPairBtn({ setIsCalendarPaired }: Props) {
   const { changeUser } = usePatchUser();
   const { getUserInfo } = useGetUserInfo();
+  const { openDialog } = useDialog();
 
   const handleGoogleSuccess = (res: any) => {
     changeUser(
@@ -30,11 +32,18 @@ function CalendarPairBtn({ setIsCalendarPaired }: Props) {
     );
   };
 
-  const handleGooglelogin = useGoogleLogin({
+  const connectGoogle = useGoogleLogin({
     onSuccess: handleGoogleSuccess,
     flow: 'auth-code',
     scope: GOOGLE_LOGIN_SCOPE,
   });
+  const handleGooglelogin = () => {
+    openDialog({
+      title: `구글 캘린더 연동`,
+      description: '연동시, 다가오는 모든 일정이 \n 구글 캘린더에 연동됩니다.',
+      onConfirm: connectGoogle,
+    });
+  };
 
   return (
     <div className={'calendar-pair-ask'}>
