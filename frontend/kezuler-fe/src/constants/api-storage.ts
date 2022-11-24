c6ea88be8b560f8e1415e2afaa5360a46064f741
ksa37 <ksa37@kaist.ac.kr>
2022-11-24 15:32:33 +0900

    Add : storage urls to pathnameList
    Add : storage server attach
    
    HotFix : remove photo, file from storage add option(not developed yet)

--- frontend/kezuler-fe/src/constants/api-storage.ts
+++ frontend/kezuler-fe/src/constants/api-storage.ts
@@ -3,17 +3,16 @@ import axios from 'axios';
 // 집
 // const HOST_ADDRESS = 'http://192.168.200.157:8888';
 // 회사
-const HOST_ADDRESS = 'http://172.30.1.55:8888';
+const HOST_ADDRESS_STORAGE = 'http://3.37.216.141';
 
 const UNAUTHORIZED_STATUS_CODE = 401;
 const FORBIDDEN_STATUS_CODE = 403;
 
 const KezulerStorageInstance = (() => {
   const instance = axios.create({
-    baseURL: HOST_ADDRESS,
+    baseURL: HOST_ADDRESS_STORAGE,
   });
   return instance;
 })();
 
-export { HOST_ADDRESS };
 export default KezulerStorageInstance;
