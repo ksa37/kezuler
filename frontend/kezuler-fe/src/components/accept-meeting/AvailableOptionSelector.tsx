bea7589b4296d057513b9ab32789856beaf4bd72
ksa37 <ksa37@kaist.ac.kr>
2022-12-02 15:15:05 +0900

    Modify : pendingevent card vote vomplete number to include declined users

--- frontend/kezuler-fe/src/components/accept-meeting/AvailableOptionSelector.tsx
+++ frontend/kezuler-fe/src/components/accept-meeting/AvailableOptionSelector.tsx
@@ -40,6 +40,7 @@ function AvailableOptionSelector({ errorMessage }: Props) {
   };
 
   const handleAllAvailableClick = () => {
+    console.log(availableTimes.length === eventTimeCandidates.length);
     if (availableTimes.length === eventTimeCandidates.length) {
       setAllAvailable(false);
       dispatch(clearAvailableTimes());
